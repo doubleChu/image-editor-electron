6aada5ec103bac69b5292658e96b647e35caf218
nomoredays@foxmail.com <nomoredays@foxmail.com>
2021-12-07 19:12:27 +0800

    fifth

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 const path = require('path')
 const sharp = require('sharp')
 const remote = require('@electron/remote')
-const { extname } = require('path/posix')
+const {
+    extname
+} = require('path/posix')
 const dialog = remote.dialog
 
 function open_img() {
     dialog.showOpenDialog(remote.getCurrentWindow(), {
         title: '选择编辑图片',
         //默认路径,默认选择的文件
-        defaultPath: '/home/chu/image-editor/src/pkgkkp.jpg',
+        defaultPath: '/home/doublechu/image-editor/src/pkgkkp.jpg',
         //过滤文件后缀
         filters: [{
-            name: 'img',
-            extensions: ['jpg', 'png', 'jpeg', 'webp']
-        }],
+                name: '图片',
+                extensions: ['jpg', 'png', 'jpeg', 'webp']
+            },
+            {
+                name: '所有文件',
+                extensions: ['*']
+            }
+        ],
         //打开按钮
         buttonLabel: '打开',
         //回调结果渲染到img标签上
@@ -31,24 +38,29 @@ function save_img() {
     let image = document.getElementById('theimg')
     dialog.showSaveDialog({
         title: '保存编辑图片',
-        defaultPath: '/home/chu/image-editor/src/',
+        defaultPath: '/home/doublechu/image-editor/src/',
         // defaultPath: path.join(__dirname, '../assets/'),
         buttonLabel: '保存',
 
         filters: [{
-            name: 'img',
-            extensions: ['jpg', 'png', 'jpeg', 'webp']
-        }],
+                name: '图片',
+                extensions: ['jpg', 'png', 'jpeg', 'webp']
+            },
+            {
+                name: '所有文件',
+                extensions: ['*']
+            }
+        ],
     }).then(result => {
         // Stating whether dialog operation was cancelled or not.
         console.log(result.canceled);
         if (!result.canceled) {
             console.log(result.filePath);
-            extname = path.extname(result.filePath)
-            if(extname == 'jpeg' || extname == 'jpg')
-                sharp(image.src).toFormat('jpg').toFile(result.filePath)
-            else{
-                sharp(image.src).toFormat(extname).toFile(result.filePath)
+            let extname = path.extname(result.filePath).substring(1)
+            if (extname == 'jpeg' || extname == 'jpg')
+                sharp('src' + image.getAttribute("src").substring(1)).toFormat('jpg').toFile(result.filePath)
+            else {
+                sharp('src' + image.getAttribute("src").substring(1)).toFormat(extname).toFile(result.filePath)
             }
         }
     }).catch(err => {
